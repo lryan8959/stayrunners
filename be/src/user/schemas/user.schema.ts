2342f997516a66819effe25e813f168c19e86b4a
Sbzda Alap Arsalan <maqsoodarsalan@outlook.com>
2024-05-01 19:08:19 +0500

    added template for mongoose schema

---
+++ be/src/user/schemas/user.schema.ts
@@ -0,0 +1,15 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+@Schema()
+export class User {
+  // @Prop({ required: true, unique: true })
+  // userId: string;
+  // @Prop()
+  // categories: Icategory[];
+  // @Prop({ required: false, default: false })
+  // newsLetterSeen: boolean;
+}
+export type UserDocument = HydratedDocument<User>;
+
+export const UserSchema = SchemaFactory.createForClass(User);
