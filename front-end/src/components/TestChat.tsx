e40177b3bb270bf8b65a74d51136e85e59c2a0ad
Sbzda Alap Arsalan <maqsoodarsalan@outlook.com>
2024-07-12 22:25:00 +0500

    Disabled button & Input field , modify code structure,

--- front-end/src/components/TestChat.tsx
+++ front-end/src/components/TestChat.tsx
@@ -9,6 +9,7 @@ import { MessagesContext } from "@/context/messages";
 import { FC, HTMLAttributes, useContext, useRef } from "react";
 import { nanoid } from "nanoid";
 import { Message } from "@/validators/message";
+import { Button } from "@/components/ui/button"
 
 const socket = io('http://localhost:5000');
 
@@ -25,6 +26,8 @@ const TestChat: React.FC = () => {
   // const [messages1, setMessages] = useState<{ sender: string, message: string, bidId: string, userRole: string }[]>([]);
   // const [messages1, setMessages] = useState<string>('');
   const [data, setData] = useState<any>();
+  const [disable,  setDisable] = useState<boolean>(false); // [disable]
+  const Accepted = "Accepted"
   useEffect(() => {
     socket.on('message', (msg) => {
       console.log(msg);
@@ -67,7 +70,6 @@ const TestChat: React.FC = () => {
       // console.log("bidId",bidId);
       // console.log("userRole",userRole);
       if (bidId !== undefined && userRole !== undefined){
-
         const id = nanoid();
         const responseMessage: Message = {
           id,
@@ -94,30 +96,47 @@ const TestChat: React.FC = () => {
     
   };
 
+  const sendMessageBtn = () => {
+    if (data !== undefined && data !== null && data !== ''){
+      const data1 = JSON.parse(data)
+      const bidId = data1.bitId;
+      const userRole = data1.userRole;
+      if (bidId !== undefined && userRole !== undefined){
+        setDisable(true);
+        const id = nanoid();
+        const responseMessage: Message = {
+          id,
+          isUserMessage: true,
+          text :Accepted
+        }
+        addMessage(responseMessage);
+// console.log("responseMessagebtn",responseMessage);
+socket.emit('message', { sender : 'User', message: Accepted, bidId, userRole});
+
+
+      }
+    }
+  }
+
   return (
+    <>
     <div className="px-4 w-[100%] borde-4 mx-auto h-10 rounded-lg bnng-red-800 flex  justify-center items-center">
   {/* <TestChat  /> */}
   <Input
    value={message}
       onChange={(e) => setMessage(e.target.value)}
        placeholder="Write a message..."
+       disabled={disable}
   />
-  <button onClick={sendMessage}>
+  <button onClick={sendMessage} disabled={disable}>
   <EnterIcon style={{ fontSize: '40px' , height: '25px', width: '25px' }}/>
   </button>
   </div>
-    // <div>
-    //   {/* <div>
-    //     {messages.map((msg, index) => (
-    //       <div key={index}>{msg.sender}: {msg.message}</div>
-    //     ))}
-    //   </div> */}
-    //   <input
-    //     value={message}
-    //     onChange={(e) => setMessage(e.target.value)}
-    //   />
-    //   <button onClick={sendMessage}>Send</button>
-    // </div>
+   <div className=" mt-2 w-[90%] mx-auto  rounded-lg flex  justify-center items-center">
+
+  <Button onClick={sendMessageBtn} disabled={disable} style={{ width: '100%' }}>Click here to Confirm Deal </Button>
+  </div>
+    </>
   );
 };
 
